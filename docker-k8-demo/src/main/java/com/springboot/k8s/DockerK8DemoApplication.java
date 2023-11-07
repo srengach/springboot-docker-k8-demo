5630b810cd21bca29a3633d8c7935c0ef83e1663
L174800 <sureshrangachari.rangachari@westpac.com.au>
2023-11-07 10:54:28 +0530

    base code

---
+++ docker-k8-demo/src/main/java/com/springboot/k8s/DockerK8DemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerK8DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerK8DemoApplication.class, args);
+	}
+
+}
---
+++ docker-k8-demo/src/main/java/com/springboot/k8s/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.springboot.k8s.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/test-docker")
+	public String getData() {
+		return "Welcome ! to Springboot deployment in Docker as Docker Image";
+	}
+}
